ac12027a0c99c2817c7cee950bcb713fc8ed0464
John Quinn <jquinn13@student01.cse.nd.edu>
2019-02-08 14:01:23 -0500

    Homework03: Activity 2

---
+++ homework03/broify.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#
+# File name: broify.sh
+# Author: John Quinn
+# Description: XXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+# NOTE: SET TABSTOP TO 2 FOR CORRECT INDENTATION.
+#
+
+##### FUNCTIONS #####
+
+usage() {
+cat << EOF
+Usage: broify.sh
+
+  -d DELIM    Use this as the comment delimiter.
+  -W          Don't strip empty lines.
+EOF
+	exit $1
+}
+
+
+##### PARSE COMMAND-LINE OPTIONS #####
+
+BOOL='#'
+DELIM='#'
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    	-h)
+				usage 1
+				;;
+			-d)
+				DELIM=$2
+				shift
+				;;
+			-W)
+				BOOL='W'
+				;;
+			*)
+				usage 1
+				;;
+    esac
+    shift
+done
+
+
+##### FILTER PIPELINE #####
+
+if [ "${BOOL}" == 'W' ]
+	then
+	sed -e "s|${DELIM}.*$||g" | sed -r 's|\s*$||'
+	else
+	sed -e "s|${DELIM}.*$||g" | sed -r '/^\s*$/d' | sed -r 's|\s*$||g'
+fi
+
